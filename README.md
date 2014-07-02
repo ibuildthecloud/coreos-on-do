1e22e35fcfe415569264944c0f9ec6daa1572231
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-07-02 02:06:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+CoreOS on Digital Ocean
+=======================
+
+This is a script to setup CoreOS on Digital Ocean.  It works by using kexec to
+first load CoreOS into memory.  Then the script repartitions the disk using the
+normal coreos-install script for local disk installation.  Then a new partition
+called DOROOT is created and we install a small Ubuntu installation.
+
+On boot of the droplet the Digital Ocean supplied kernel will find the small
+Ubuntu installation which will immediately kexec into CoreOS and reload the
+environment with the CoreOS kernel and ramdisk.  After that it's all 100%
+CoreOS.
+
+I haven't tested this fully, it seems like CoreOS auto-update will work, but I
+haven't had enough time to try it out.
+
+Installation
+============
+
+Start with an Ubuntu 14.04 droplet installed **with an SSH key**.  The SSH key
+is important as it's how you're going to log into the CoreOS installation.
+I've tried this on nyc2, for some reason sfo1 didn't work as well.  Once
+booted run
+
+    wget https://raw.githubusercontent.com/ibuildthecloud/coreos-on-do/master/coreos-on-do.sh
+    sudo bash coreos-on-do.sh
+
+That will run a bunch of stuff and then reboot the droplet.  The last line
+you'll see is `kexec -e`.  Now go to the Digital Ocean console for your
+droplet.  Run the following on the web console.
+
+    sudo mount LABEL=DOROOT /mnt
+    sudo /mnt/root/stage2.sh
+
+That will do a bunch of stuff and reboot.  For some reason the networkd config
+doesn't seem to take effect on the first boot.  So now go to the Digital Ocean
+console and do a power cycle on your droplet.  If all is swell you should be
+able to SSH into your newly installed CoreOS.
---
+++ coreos-on-do.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+set -e
+set -x
+
+cd $(dirname $0)
+
+stage1()
+{
+    cd /root
+    cat > cloud-config.yaml << EOF
+#cloud-config
+
+write_files:
+  - path: /etc/systemd/network/do.network
+    permissions: 0644
+    content: |
+      [Match]
+      Name=ens3
+      
+      [Network]
+      Address=$(ip addr show dev eth0 | grep 'inet.*eth0' | awk '{print $2}')
+      Gateway=$(route | grep default | awk '{print $2}')
+      DNS=8.8.4.4
+      DNS=8.8.8.8
+
+ssh_authorized_keys:
+  - $(cat /root/.ssh/authorized_keys | head -1)
+EOF
+
+    wget http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
+    wget http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
+
+    cp $0 stage2.sh
+    chmod +x stage2.sh
+
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get install -y kexec-tools
+    kexec -l coreos_production_pxe.vmlinuz --initrd coreos_production_pxe_image.cpio.gz --append='coreos.autologin=tty1'
+    kexec -e
+}
+
+stage2()
+{
+    tar cvzf /var/tmp/modules.tar.gz -C /mnt lib/modules
+    cp $0 /var/tmp
+    cp $(dirname $0)/cloud-config.yaml /var/tmp
+    chmod +x /var/tmp/$(basename $0)
+    exec /var/tmp/$(basename $0) stage3
+}
+
+stage3()
+{
+    if cat /proc/mounts | awk '{print $2}' | grep -q '^/mnt$'; then
+        umount /mnt
+    fi
+
+    coreos-cloudinit --from-file=cloud-config.yaml
+    systemctl restart systemd-networkd
+
+    wget --no-check-certificate https://raw.github.com/coreos/init/master/bin/coreos-install
+    chmod +x coreos-install
+    ./coreos-install -C alpha -d /dev/vda -c cloud-config.yaml
+
+    cgpt repair /dev/vda
+    parted -s -- /dev/vda mkpart DOROOT ext4 -500M -0
+    ID=$(parted -sm /dev/vda p | grep DOROOT | cut -f1 -d:)
+    mkfs.ext4 -L DOROOT /dev/vda${ID}
+
+    mount LABEL=DOROOT /mnt
+    curl -s http://cdimage.ubuntu.com/ubuntu-core/releases/14.04/release/ubuntu-core-14.04-core-amd64.tar.gz | tar xvzf - -C /mnt
+    cp /etc/resolv.conf /mnt/etc/resolv.conf
+
+    export DEBIAN_FRONTEND=noninteractive
+    chroot /mnt apt-get update
+    chroot /mnt apt-get install -y kexec-tools
+
+    if [ ! -e /mnt/sbin/init.real ]; then
+        mv /mnt/sbin/init{,.real}
+    fi
+
+    cat > /mnt/sbin/init << "EOF"
+#!/bin/bash
+
+if [ ! -e /boot/syslinux/vmlinuz-boot_kernel ]; then
+        mount -o ro LABEL=EFI-SYSTEM /boot
+fi
+
+KERNEL=$(grep '^[[:space:]]*kernel' /boot/syslinux/boot_kernel.cfg | awk '{print $2}')
+APPEND=$(grep '^[[:space:]]*append' /boot/syslinux/boot_kernel.cfg | sed 's/^[[:space:]]*append //g')
+
+kexec -l /boot/syslinux/$KERNEL --append="$APPEND"
+kexec -e
+
+# In case something goes wrong, we drop to a shell, normally kexec would reboot the system
+bash
+EOF
+
+    chmod +x /mnt/sbin/init
+
+    tar xvzf modules.tar.gz -C /mnt
+    umount /mnt
+
+    echo Rebooting
+    reboot
+}
+
+if [ $(id -u) != 0 ]; then
+    echo Run as root
+    exit 1
+fi
+
+if [ "$0" = "bash" ]; then
+    echo "Download this script, don't pipe it to bash"
+    exit 1
+fi
+
+. /etc/lsb-release
+
+if [ "$DISTRIB_ID" = "Ubuntu" ]; then
+    stage1
+elif [ "$1" = "stage3" ]; then
+    stage3
+else
+    stage2
+fi
